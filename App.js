9b09ce49f358bd8be63f5de687aa449502afe632
AishwaryaRJangam <64888200+AishwaryaRJangam@users.noreply.github.com>
2020-07-16 19:27:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+// import TrackCreateScreen from './src/screens/TrackCreateScreen';
+// import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import LocationScreen from './src/screens/LocationScreen';
+import CameraScreen from './src/screens/CameraScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
+
+import { Provider as AuthProvider } from './src/context/AuthContext';
+import { setNavigator } from './src/navigationRef';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
+import { Provider as LocationProvider } from './src/context/LocationContext';
+import { Provider as TrackProvider } from './src/context/TrackContext';
+import { FontAwesome } from '@expo/vector-icons';
+
+const trackListFlow = createStackNavigator({
+    Restaurents : SearchScreen,
+    ResultsShow: ResultsShowScreen,
+});
+
+trackListFlow.navigationOptions = {
+  title: 'Restaurents',
+  tabBarIcon: <FontAwesome name="th-list" size={20} />,
+};
+
+const switchNavigator = createSwitchNavigator({
+  ResolveAuth: ResolveAuthScreen,
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow,
+    Camera: CameraScreen,
+    Location: LocationScreen,
+    Account: AccountScreen,
+  }),
+});
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <App
+            ref={(navigator) => {
+              setNavigator(navigator);
+            }}
+          />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
+  );
+};
